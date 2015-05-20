0521ad668bcfbc350e3c5e9ba89572012892aad4
jahan558 <shahnazjahan558@gmail.com>
2015-05-20 11:54:40 +0600

    ok
    done

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function() {
+
+	$('#diagram-id-1').circleDiagram({
+		"percent": "50",
+		"size": "120",
+		"borderWidth": "20",
+		"bgFill": "#76c7c0",
+		"frFill": "#e2534b",
+		"textSize": "36",
+		"textColor": "#7f8c8c"
+	});
+
+	$('#diagram-id-2').circleDiagram({
+		"percent": "70",
+		"size": "120",
+		"borderWidth": "20",
+		"bgFill": "#76c7c0",
+		"frFill": "#e2534b",
+		"textSize": "36",
+		"textColor": "#7f8c8c"
+	});
+	
+	$('#diagram-id-3').circleDiagram({
+		"percent": "80",
+		"size": "120",
+		"borderWidth": "20",
+		"bgFill": "#76c7c0",
+		"frFill": "#e2534b",
+		"textSize": "36",
+		"textColor": "#7f8c8c"
+	});
+	
+	$('#diagram-id-4').circleDiagram({
+		"percent": "100",
+		"size": "120",
+		"borderWidth": "20",
+		"bgFill": "#76c7c0",
+		"frFill": "#e2534b",
+		"textSize": "36",
+		"textColor": "#7f8c8c"
+	});
+});
